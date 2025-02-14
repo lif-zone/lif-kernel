62bc89cd2cc89e684e2bd2751737692f10da8beb
Go Jungo <jungo@bright.center>
2025-02-14 20:21:26 +0200

    update

---
+++ app-os/os_boot.js
@@ -0,0 +1,7 @@
+import React from 'react';
+window.React = React;
+console.log('index_boot.js started');
+let app = (await import('pages/index.tsx')).default;
+console.log('index_boot.js complete');
+export default app;
+
