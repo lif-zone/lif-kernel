0218d1e102387cbc9a9a515257f582ae96dfd9b6
Bright Life <bright@bright.center>
2026-05-22 20:50:33 +0300

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let root = import.meta.dirname;
 let map = {};
 map['/lif-kernel'] = '.';
 // local dev
-map['/lif-basic'] = './lif-basic';
+map['/lif-basic'] = '../lif-basic';
 map['/lif-os'] = '../lif-os';
 map['/lif-coin'] = '../lif-coin';
 map['/lif-net'] = '../lif-net';
