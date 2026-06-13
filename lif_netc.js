88621e77a1b20801b53c5d8cfce53eef03a9eb0f
Bright Life <bright@bright.center>
2026-06-14 01:11:36 +0300

    update

--- lif_netc.js
+++ lif_netc.js
@@ -16,66 +16,58 @@ export class tcp_sock extends EventEmitter {
     this.remotePort = 0;
     this.closed = false;
   }
-  connect(port, host){
+  async connect(port, host){
     this.remoteAddress = host;
     this.remotePort = port;
-    (async()=>{
-      try {
-        const sock = new rpc_sock();
-        this.sock = sock;
-        // Data
-        sock.method('data', ({data})=>{
-          data = Buffer.from(data, 'hex');
-          if (this.paused){
-            this.recvBuffer.push(data);
-            return;
-          }
-          this.bytesRead += data.length;
-          this.emit('data', data);
-        });
-        sock.method('error', ({message, code})=>{
-          const err = new Error(message);
-          err.code = code;
-          this.emit('error', err);
-        });
-        sock.method('close', ()=>{
-          if (this.closed)
-            return;
-          this.closed = true;
-          this.emit('close');
-        });
-        sock.on('close', ()=>{
-          if (this.closed)
-            return;
-          this.closed = true;
-          this.emit('close');
-        });
-        const res = await sock.connect(this.rpc, 'tcp_connect', {host, port});
-        this.remoteAddress = res.addr;
-        this.remotePort = res.port;
-        // Flush buffered writes
-        for (const {data, cb} of this.sendBuffer){
-          sock.notify('data', {data: data.toString('hex')});
-          if (cb)
-            cb();
-        }
-        this.sendBuffer = [];
-        this.emit('connect');
-      } catch(err){
-        this.closed = true;
-        this.emit('error', err instanceof Error ? err : new Error(''+err));
+    const sock = new rpc_sock();
+    this.sock = sock;
+    sock.method('data', ({data})=>{
+      data = Buffer.from(data, 'hex');
+      if (this.paused){
+        this.recvBuffer.push(data);
+        return;
       }
-    })();
+      this.bytesRead += data.length;
+      this.emit('data', data);
+    });
+    sock.method('error', ({message, code})=>{
+      const err = new Error(message);
+      err.code = code;
+      this.emit('error', err);
+    });
+    sock.method('close', ()=>{
+      if (this.closed)
+        return;
+      this.closed = true;
+      this.emit('close');
+    });
+    sock.on('close', ()=>{
+      if (this.closed)
+        return;
+      this.closed = true;
+      this.emit('close');
+    });
+    const res = await sock.connect(this.rpc, 'tcp_connect', {host, port});
+    if (res.error){
+      this.closed = true;
+      this.emit('error', res.error);
+      return;
+    }
+    this.remoteAddress = res.addr;
+    this.remotePort = res.port;
+    // Flush buffered writes
+    for (const {data} of this.sendBuffer)
+      sock.notify('data', {data: data.toString('hex')});
+    this.sendBuffer = [];
+    this.emit('connect');
   }
-  write(data, callback){
+  write(data){
     this.bytesWritten += data.length;
     if (!this.sock){
-      this.sendBuffer.push({data, cb: callback});
+      this.sendBuffer.push({data});
       return true;
     }
     this.sock.notify('data', {data: data.toString('hex')});
-    if (callback)
-      callback();
     return true;
   }
   setKeepAlive(enable, delay){
@@ -84,14 +76,14 @@ export class tcp_sock extends EventEmitter {
   setNoDelay(enable){
     this.sock?.notify('no_delay', {enable});
   }
-  setTimeout(timeout, callback){
+  setTimeout(timeout, fn){
     this.sock?.notify('set_timeout', {timeout});
-    if (callback)
-      this.on('timeout', callback);
+    if (fn)
+      this.on('timeout', fn);
   }
   pause(){
     this.paused = true;
-    this.sock?.notify('pause');
+    this.sock.notify('pause');
   }
   resume(){
     this.paused = false;
@@ -101,7 +93,7 @@ export class tcp_sock extends EventEmitter {
       this.bytesRead += data.length;
       this.emit('data', data);
     }
-    this.sock?.notify('resume');
+    this.sock.notify('resume');
   }
   destroy(){
     if (this.closed)
