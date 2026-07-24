b338a3638ac062b069b8189fbfc5b15e6cd61e5f
Bright Life <bright@bright.center>
2026-07-24 17:01:58 +0300

    update

--- serve.js
+++ serve.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 import server from './server_lib.js';
 import fs from 'fs';
-let root = import.meta.dirname;
+import process from 'process';
+let root = process.cwd();
 let map = {};
 let pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 map['/lif-kernel'] = 'node_modules/lif-kernel';
-map['/'+pkg.name] = '.';
 map['/index.html'] = {path: 'node_modules/lif-kernel/serve_index.html',
   tr_fn: s=>s.replace(/__WEBAPP__/g, '/'+pkg.name+'/')};
+map['/'+pkg.name] = '.';
 server({map, root});
