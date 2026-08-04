0a83fc23dd5d50b148733ac70ef70dbad5e5b8f9
Bright Life <bright@bright.center>
2026-08-04 09:02:57 +0300

    added site.center

--- web/ssl_s.js
+++ web/ssl_s.js
@@ -215,6 +215,7 @@ export async function do_ssl(opt){
   dns_s.set_domains({
     'pub.site': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     'lif.site': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
+    'site.center': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     'bright.life': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     'zon.life': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     // 'lif.zone': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
