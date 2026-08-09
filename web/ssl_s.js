661d402e9f5ed042b5fd724e5715d599fab36d3c
Bright Life <bright@bright.center>
2026-08-09 23:25:53 +0300

    added domain

--- web/ssl_s.js
+++ web/ssl_s.js
@@ -229,6 +229,7 @@ export async function do_ssl(opt){
     'lifnet.net': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     'zon.life': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     'lifcoin.org': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
+    'lif-coin.com': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     'lif.zone': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']}, // not activated
     'arik.center': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
     'venao.center': {ssl: true, ip: '50.7.176.34', ns: ['ns1', 'ns2']},
