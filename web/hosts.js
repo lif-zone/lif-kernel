60896ab30afc330ca7bb20dd7ed99cd2cae86a6f
Bright Life <bright@bright.center>
2026-08-13 23:09:44 +0300

    update

--- web/hosts.js
+++ web/hosts.js
@@ -9,6 +9,7 @@ function add_domains_hosts({ip, domains, hosts}){
   let ips = qw(ip);
   domains = qw(domains);
   hosts = qw(hosts);
+  console.log('all', ip, domains, hosts);
   for (let ip of ips){
     let _host = host[ip] ||= {domains: {}};
     for (let d of domains){
