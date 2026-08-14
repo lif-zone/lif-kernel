73b05baf55308e49654a4d6764bf2970fa0eee58
Bright Life <bright@bright.center>
2026-08-14 16:20:49 +0300

    update

--- web/server_lib.js
+++ web/server_lib.js
@@ -199,6 +199,11 @@ function http_listener(req, res){
     return http_pipe({req, res, url: `http://localhost:8432/blockstream/${v.rest}`});
   if (v=map_uri({uri, opt: g_opt}))
     return res_send_file(res, v);
+  let dest = req.headers['sec-fetch-dest'];
+  let mode = req.headers['sec-fetch-mode'];
+  let lif_kernel = g_opt.map['/lif-kernel'];
+  if (dest=='document' || mode=='navigate') // SPA
+    return res_send_file(res, lif_kernel+'/index.html');
   return res_err(res, 404, 'no map found');
 }
 
