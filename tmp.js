b8225c296ea333877f35b6a977209ca292e239dc
LIF Deploy <lifdeploy@lif.zone>
2026-08-21 20:29:00 +0300

    update

--- tmp.js
+++ tmp.js
@@ -1,21 +1,48 @@
 let lif = globalThis.$lif;
 
+function html_elm_frag(html){
+  const template = document.createElement('template');
+  template.innerHTML = html;
+  return template.content.children; // returns HTMLCollection
+}
+
+async function input_webapp(){
+  let body = document.querySelector('body');
+  const e = html_elm_frag(`
+    <h1>Enter LIF site: <input></h1>
+    <h2>
+      git example: git://github.com/lif-zone/lif-basic
+      npm example: npm:lif-basic@1.3.0
+    </h2>
+  `);
+  for (let c; c = e[0];)
+    body.appendChild(c);
+}
+
+let clean_url = false;
 async function tmp_webapp_resolve(){
   let q = new URLSearchParams(location.search);
   let v, webapp;
   let ls = localStorage.getItem('tmp_webapp');
-  if (v=q.get('webapp')){
-    if (ls)
+  let qs = q.get('webapp');
+  if (qs){
+    if (ls && qs!=ls)
       console.warn('changing webapp '+ls+' -> '+v);
-    webapp = v;
+    webapp = qs;
     localStorage.setItem('tmp_webapp', webapp);
+    if (clean_url){
+      q.delete('webapp');
+      let _qs = ''+q;
+      if (_qs=='?')
+        _qs = '';
+      window.location = location.origin+location.pathname+_qs+location.hash;
+      return;
+    }
   } else if (ls)
     webapp = ls;
-  else
-    console.error('ask user to enter webapp');
-  if (!webapp)
-    return;
-  return await lif.boot.boot_app({lif: {webapp}});
+  if (webapp)
+    return await lif.boot.boot_app({lif: {webapp}});
+  input_webapp();
 }
 await tmp_webapp_resolve();
 
