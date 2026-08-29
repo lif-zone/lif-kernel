31330e5836bd9395e18c42eab7b67156180804e8
LIF Deploy <lifdeploy@lif.zone>
2026-08-29 17:22:21 +0300

    update

--- kernel.js
+++ kernel.js
@@ -1715,7 +1715,7 @@ async function _kernel_fetch(event){
   // root document only.
   if (str.is(request.cache, 'no-cache', 'reload') &&
     (request.mode=='navigate' || 
-    str.is(request.destination, 'document', 'iframe')))
+    str.is(request.destination, 'document', 'iframe', 'frame')))
   { 
     console.log('cache_refresh', request.cache, request.mode, 
       request.destination, url);
